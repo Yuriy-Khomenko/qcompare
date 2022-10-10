e50d34c167fdf02080bcd3e4bb48fe3575c33b9d
yuriy.kh <yuriy.kh@siteplus.com>
2022-10-11 01:09:44 +0300

    small improvements

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 'use strict';
-
-const __Generator__ = Object.getPrototypeOf(function*(){});
-const __qkeys__ = Object.keys;
-const __qhop__ = Object.prototype.hasOwnProperty;
+const G = Object.getPrototypeOf(function*(){});
+const keys = Object.keys;
+const hop = Object.prototype.hasOwnProperty;
 
 function qcompare(a, b) {
     if(a === b)return true;
@@ -11,27 +10,22 @@ function qcompare(a, b) {
       if(c !== b.constructor)return false;
       switch(c){
         case Object:
-          let t,i,x,p =__qkeys__(b), g = 0;
+          let t,i,p=keys(b),g=0;
           for(i in a){
-            if(__qhop__.call(a, i)){
-              x = a[i];
+            if(hop.call(a,i)){
               t = b[i];
-              if(x !== t){
-                if(!qcompare(x, t))return false;
-              }else if(t === undefined && i !== p[g] && !__qhop__.call(b, i))return false;
+              if(a[i] !== t && !qcompare(a[i],t)){
+                return false;
+              }else if(t === undefined && i !== p[g] && !hop.call(b,i))return false;
               g++;
             }
           }
-          if(g === p.length)return true;
-          return false;
+          return g === p.length;
         case Array:
           let q = a.length;
           if(q !== b.length)return false;
-          while(q--){
-            if(a[q] === b[q])continue;
-            if(qcompare(a[q], b[q]) === false)return false;
-          }
-          return true;
+          while(q-- && a[q] === b[q] );
+          return !~q || qcompare(a[q],b[q]);
         case Date:
           a = a.getTime();
           b = b.getTime();
@@ -42,7 +36,7 @@ function qcompare(a, b) {
           for(c of a){
             m = e.next().value;
             if(m[0] !== c[0]) if(m[0] !== m[0] && c[0] !== c[0]){}else return false;
-            if(m[1] !== c[1]) if(!qcompare(c[1], m[1]))return false;
+            if(m[1] !== c[1] && !qcompare(c[1],m[1]))return false;
           }
           return true;
         case Set:
@@ -50,83 +44,66 @@ function qcompare(a, b) {
           let v,h,r = b.values();
           for(v of a){
             h = r.next().value;
-            if(h !== v) if(!qcompare(v, h))return false;
+            if(h !== v && !qcompare(v,h))return false;
           }
           return true;
         case RegExp:
           return a.source === b.source && a.flags === b.flags;
-        case String:
-        case Number:
-        case Boolean:
-        //case BigInt:
-          return a.valueOf() === b.valueOf();
+        case undefined:
+          let u,y = 0;
+          for(u in a){
+            if(!(u in b) || !qcompare(a[u],b[u]))return false;
+            y++;
+          }
+          return y === keys(b).length;
+        case ArrayBuffer:
+        //case SharedArrayBuffer:
+          let o = a.byteLength;
+          if(o !== b.byteLength)return false;
+          o = a.byteLength >> 3;
+          let s=o*8,d=new Float64Array(a,0,o),f=new Float64Array(b,0,o);
+          while(o-- && d[o] === f[o]);
+          if(~o)return false;
+          a = new Uint8Array(a,s);
+          b = new Uint8Array(b,s);
+          o = a.length;
+          while(o-- && a[o] === b[o]);
+          return !~o;          
+        case DataView:
+          if(a.buffer === b.buffer)return true;
+          let w = a.byteLength;
+          if(w !== b.byteLength)return false;
+          while(w > 7){w -= 8; if(a.getFloat64(w) !== b.getFloat64(w))return false;};
+          while(w-- && a.getInt8(w) === b.getInt8(w));
+          return !~w;
         case Promise:
         case Symbol:
         case WeakMap:
         case WeakSet:
-        case __Generator__:
+        case G:
           return a === b;
-        case undefined:
-          let u,y = 0;
-          for(u in a){
-            if(!(u in b))return false;
-            if(!qcompare(a[u], b[u]))return false;
-            y++;
-          }
-          if(y === __qkeys__(b).length)return true;
-          return false;
-        case DataView:
-          if(a.buffer===b.buffer)return true; //? a.byteOffset === b.byteOffset
-          let w=a.byteLength;
-          if(w!==b.byteLength)return false;
-          while(w>7){w-=8;if(a.getFloat64(w)!==b.getFloat64(w))return false;};
-          while(w--&&a.getInt8(w)===b.getInt8(w));
-          return !~w;
-        case ArrayBuffer:
-        //case SharedArrayBuffer:
-          let o=a.byteLength;
-          if(o!==b.byteLength)return false;
-          o=a.byteLength>>3;
-          let s=o*8,d=new Float64Array(a,0,o),f=new Float64Array(b,0,o);
-          while(o--&&d[o]===f[o]);
-          if(~o)return false;
-          a=new Uint8Array(a,s);
-          b=new Uint8Array(b,s);
-          o=a.length;
-          while(o--&&a[o]=== b[o]);
-          return !~o;
-        case Error:
-        case RangeError:
-        case EvalError:
-        case ReferenceError:
-        case SyntaxError:
-        case TypeError:
-        case URIError:
-        //case InternalError:
-        //case AggregateError:
-          return a.message === b.message && a.name === b.name;
         default:
           if(a.BYTES_PER_ELEMENT){
+            if(!b.BYTES_PER_ELEMENT)return false;
             let s = a.length;
             if(s !== b.length)return false;
-            while(s--){if(a[s] !== b[s])return false;}
-            return true;
+            while(s-- && a[s] === b[s]);
+            return !~s;
           }
-          let k,z,l,n = __qkeys__(b), j = 0;
+          if(a.valueOf) return qcompare(a.valueOf(),b.valueOf());
+          if(a.toString) return a.toString() === b.toString();
+          let k,z,n=keys(b),j=0;
           for(z in a){
-            if(__qhop__.call(a, z)){
-              l = a[z];
+            if(hop.call(a, z)){
               k = b[z];
-              if(l !== k){
-                if(!qcompare(l, k))return false;
-              }else if(l === undefined && z !== n[j] && !__qhop__.call(b, z))return false;
+              if(a[z] !== k && !qcompare(a[z],k)){
+                return false;
+              }else if(l === undefined && z !== n[j] && !hop.call(b,z))return false;
               j++;
             }
           }
-          if(j === n.length)return true;
-          return false;
+          return j === n.length;
       }
-      return false;
     }
     return a !== a && b !== b;
   }
