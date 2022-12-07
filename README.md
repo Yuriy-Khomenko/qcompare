186c016879d23acfe4e72104f4d5b7ffc84846e4
yuriy.kh <yuriy.kh@siteplus.com>
2022-12-08 00:16:06 +0200

    update benchmarks and small changes logic

--- README.md
+++ README.md
@@ -34,65 +34,64 @@ qcompare( { bar: 1 }, { bar: 2 } ); // false
 For more balanced testing of performance, tests of other projects were used
 
 ```
-Nodejs - v13.3.0
-Date - 09.12.2019
+Nodejs - v16.8.0
+Date - 07.12.2022
 ```
 
-[fast-deep-equal](https://github.com/epoberezkin/fast-deep-equal) project test result
+[fast-deep-equal v3.1.3](https://github.com/epoberezkin/fast-deep-equal) project test result
 
 ```
-qcompare x 260,753 ops/sec ±0.20% (91 runs sampled)
-fast-deep-equal x 189,710 ops/sec ±0.57% (91 runs sampled)
-fast-deep-equal/es6 x 169,133 ops/sec ±0.22% (91 runs sampled)
-fast-equals x 176,720 ops/sec ±0.29% (89 runs sampled)
-nano-equal x 136,845 ops/sec ±0.17% (92 runs sampled)
-shallow-equal-fuzzy x 127,262 ops/sec ±0.18% (93 runs sampled)
-underscore.isEqual x 81,972 ops/sec ±0.49% (95 runs sampled)
-lodash.isEqual x 29,343 ops/sec ±0.80% (92 runs sampled)
-deep-equal x 3,182 ops/sec ±0.40% (93 runs sampled)
-deep-eql x 29,602 ops/sec ±0.95% (95 runs sampled)
-ramda.equals x 9,123 ops/sec ±0.50% (94 runs sampled)
-util.isDeepStrictEqual x 48,067 ops/sec ±0.52% (94 runs sampled)
-assert.deepStrictEqual x 1,129 ops/sec ±0.30% (88 runs sampled)
-
+qcompare x 328,360 ops/sec ±0.27% (92 runs sampled)
+fast-deep-equal x 275,325 ops/sec ±0.54% (92 runs sampled)
+fast-deep-equal/es6 x 229,257 ops/sec ±0.43% (97 runs sampled)
+fast-equals x 257,231 ops/sec ±0.31% (94 runs sampled)
+nano-equal x 173,235 ops/sec ±0.29% (96 runs sampled)
+shallow-equal-fuzzy x 119,923 ops/sec ±0.45% (94 runs sampled)
+underscore.isEqual x 79,648 ops/sec ±0.77% (93 runs sampled)
+lodash.isEqual x 36,797 ops/sec ±0.65% (96 runs sampled)
+deep-equal x 87.99 ops/sec ±4.44% (42 runs sampled)
+deep-eql x 23,791 ops/sec ±0.78% (93 runs sampled)
+ramda.equals x 10,551 ops/sec ±1.65% (95 runs sampled)
+util.isDeepStrictEqual x 49,437 ops/sec ±0.58% (96 runs sampled)
+assert.deepStrictEqual x 579 ops/sec ±0.40% (93 runs sampled)
 The fastest is qcompare
 ```
 
-[fast-equals](https://github.com/planttheidea/fast-equals) project test result
+[fast-equals v4.0.3](https://github.com/planttheidea/fast-equals) project test result
 
 |                        | Ops / sec |
 | ---------------------- | --------- |
-| qcompare               | 6 626,798 |
-| fast-equals            | 4 500,993 |
-| fast-equals (circular) | 2 985,654 |
-| deep-eql               | 2 282,853 |
-| react-fast-compare     | 764,074   |
-| nano-equal             | 716,619   |
-| fast-deep-equal        | 707,519   |
-| deep-equal             | 426,977   |
-| underscore.isEqual     | 381,921   |
-| lodash.isEqual         | 263,044   |
-| assert.deepStrictEqual | 9,504     |
+| qcompare               | 5 492,983 |
+| fast-equals            | 3 495,332 |
+| deep-eql               | 1 880,796 |
+| fast-equals (circular) | 1 859,143 |
+| fast-deep-equal        | 1 783,733 |
+| react-fast-compare     | 1 556,482 |
+| nano-equal             |   652,388 |
+| underscore.isEqual     |   445,516 |
+| lodash.isEqual         |   219,295 |
+| assert.deepStrictEqual |    10,583 |
+| deep-equal             |     0,671 |
 
-[dequal](https://github.com/lukeed/dequal) project test result
+[dequal v2.0.3](https://github.com/lukeed/dequal) project test result
 
 ```
 (basic) Benchmark:
-  assert.deepStrictEqual x 131,922 ops/sec ±2.54% (84 runs sampled)
-  util.isDeepStrictEqual x 136,767 ops/sec ±0.63% (87 runs sampled)
-  fast-deep-equal        x 495,419 ops/sec ±0.68% (89 runs sampled)
-  lodash.isEqual         x 128,821 ops/sec ±0.73% (89 runs sampled)
-  nano-equal             x 432,401 ops/sec ±0.50% (89 runs sampled)
-  dequal/lite            x 663,637 ops/sec ±0.83% (89 runs sampled)
-  dequal                 x 635,513 ops/sec ±0.57% (88 runs sampled)
-  qcompare               x 919,345 ops/sec ±1.13% (86 runs sampled)
+  assert.deepStrictEqual x 242,497 ops/sec ±0.90% (92 runs sampled)
+  util.isDeepStrictEqual x 243,227 ops/sec ±0.88% (95 runs sampled)
+  fast-deep-equal        x 1,283,468 ops/sec ±0.44% (95 runs sampled)
+  lodash.isEqual         x 265,328 ops/sec ±0.86% (94 runs sampled)
+  nano-equal             x 969,216 ops/sec ±1.06% (95 runs sampled)
+  dequal/lite            x 1,350,907 ops/sec ±2.40% (93 runs sampled)
+  dequal                 x 1,360,938 ops/sec ±0.87% (94 runs sampled)
+  qcompare               x 2,216,972 ops/sec ±0.70% (95 runs sampled)
 
 (complex) Benchmark(excluding object-keys for the Map):
-  assert.deepStrictEqual x 53,397 ops/sec ±0.84% (85 runs sampled)
-  util.isDeepStrictEqual x 53,838 ops/sec ±0.83% (88 runs sampled)
-  lodash.isEqual         x 19,453 ops/sec ±7.39% (86 runs sampled)
-  dequal                 x 266,232 ops/sec ±0.90% (87 runs sampled)
-  qcompare               x 538,806 ops/sec ±0.33% (85 runs sampled)
+  assert.deepStrictEqual x 112,922 ops/sec ±0.95% (93 runs sampled)
+  util.isDeepStrictEqual x 113,051 ops/sec ±1.08% (95 runs sampled)
+  lodash.isEqual         x 52,685 ops/sec ±1.18% (89 runs sampled)
+  dequal                 x 467,948 ops/sec ±0.51% (95 runs sampled)
+  qcompare               x 1,158,099 ops/sec ±1.43% (92 runs sampled)
 ```
 
 ### License
--- index.js
+++ index.js
@@ -3,6 +3,110 @@ const G = Object.getPrototypeOf(function*(){});
 const keys = Object.keys;
 const hop = Object.prototype.hasOwnProperty;
 
+
+function _(a, b) {
+    if(typeof a === 'object' && typeof b === 'object' && a !== null && b !== null){
+      let c = a.constructor;
+      if(c !== b.constructor)return false;
+      switch(c){
+        case Object:
+          let t,i,p=keys(b),g=0;
+          for(i in a){
+            if(hop.call(a,i)){
+              t = b[i];
+              if(a[i] !== t && !_(a[i],t)){
+                return false;
+              }else if(t === undefined && i !== p[g] && !hop.call(b,i))return false;
+              g++;
+            }
+          }
+          return g === p.length;
+        case Array:
+          let q = a.length;
+          if(q !== b.length)return false;
+          while(q-- && a[q] === b[q] );
+          return !~q || _(a[q],b[q]);
+        case Date:
+          a = a.getTime();
+          b = b.getTime();
+          return a === b || (a !== a && b !== b);
+        case Map:
+          if(a.size !== b.size || a.entries !== b.entries)return false;
+          let c,m,e = b.entries();
+          for(c of a){
+            m = e.next().value;
+            if(m[0] !== c[0]) if(m[0] !== m[0] && c[0] !== c[0]){}else return false;
+            if(m[1] !== c[1] && !_(c[1],m[1]))return false;
+          }
+          return true;
+        case Set:
+          if(a.size !== b.size || a.values !== b.values)return false;
+          let v,h,r = b.values();
+          for(v of a){
+            h = r.next().value;
+            if(h !== v && !_(v,h))return false;
+          }
+          return true;
+        case RegExp:
+          return a.source === b.source && a.flags === b.flags;
+        case undefined:
+          let u,y = 0;
+          for(u in a){
+            if(!(u in b) || (a[u] !== b[u] && !_(a[u],b[u])))return false;
+            y++;
+          }
+          return y === keys(b).length;
+        case ArrayBuffer:
+          let o = a.byteLength;
+          if(o !== b.byteLength)return false;
+          o = a.byteLength >> 3;
+          let s=o*8,d=new Float64Array(a,0,o),f=new Float64Array(b,0,o);
+          while(o-- && d[o] === f[o]);
+          if(~o)return false;
+          a = new Uint8Array(a,s);
+          b = new Uint8Array(b,s);
+          o = a.length;
+          while(o-- && a[o] === b[o]);
+          return !~o;          
+        case DataView:
+          if(a.buffer === b.buffer)return true;
+          let w = a.byteLength;
+          if(w !== b.byteLength)return false;
+          while(w > 7){w -= 8; if(a.getFloat64(w) !== b.getFloat64(w))return false;};
+          while(w-- && a.getInt8(w) === b.getInt8(w));
+          return !~w;
+        case Promise:
+        case Symbol:
+        case WeakMap:
+        case WeakSet:
+        case G:
+          return a === b;
+        default:
+          if(a.BYTES_PER_ELEMENT){
+            if(!b.BYTES_PER_ELEMENT)return false;
+            let s = a.length;
+            if(s !== b.length)return false;
+            while(s-- && a[s] === b[s]);
+            return !~s;
+          }
+          if(a.valueOf) return a.valueOf() === b.valueOf();
+          if(a.toString) return a.toString() === b.toString();
+          let k,z,n=keys(b),j=0;
+          for(z in a){
+            if(hop.call(a, z)){
+              k = b[z];
+              if(a[z] !== k && !_(a[z],k)){
+                return false;
+              }else if(l === undefined && z !== n[j] && !hop.call(b,z))return false;
+              j++;
+            }
+          }
+          return j === n.length;
+      }
+    }
+    return a !== a && b !== b;
+  }
+
 function qcompare(a, b) {
     if(a === b)return true;
     if(typeof a === 'object' && typeof b === 'object' && a !== null && b !== null){
@@ -14,7 +118,7 @@ function qcompare(a, b) {
           for(i in a){
             if(hop.call(a,i)){
               t = b[i];
-              if(a[i] !== t && !qcompare(a[i],t)){
+              if(a[i] !== t && !_(a[i],t)){
                 return false;
               }else if(t === undefined && i !== p[g] && !hop.call(b,i))return false;
               g++;
@@ -25,7 +129,7 @@ function qcompare(a, b) {
           let q = a.length;
           if(q !== b.length)return false;
           while(q-- && a[q] === b[q] );
-          return !~q || qcompare(a[q],b[q]);
+          return !~q || _(a[q],b[q]);
         case Date:
           a = a.getTime();
           b = b.getTime();
@@ -36,7 +140,7 @@ function qcompare(a, b) {
           for(c of a){
             m = e.next().value;
             if(m[0] !== c[0]) if(m[0] !== m[0] && c[0] !== c[0]){}else return false;
-            if(m[1] !== c[1] && !qcompare(c[1],m[1]))return false;
+            if(m[1] !== c[1] && !_(c[1],m[1]))return false;
           }
           return true;
         case Set:
@@ -44,7 +148,7 @@ function qcompare(a, b) {
           let v,h,r = b.values();
           for(v of a){
             h = r.next().value;
-            if(h !== v && !qcompare(v,h))return false;
+            if(h !== v && !_(v,h))return false;
           }
           return true;
         case RegExp:
@@ -52,7 +156,7 @@ function qcompare(a, b) {
         case undefined:
           let u,y = 0;
           for(u in a){
-            if(!(u in b) || !qcompare(a[u],b[u]))return false;
+            if(!(u in b) || (a[u] !== b[u] && !_(a[u],b[u])))return false;
             y++;
           }
           return y === keys(b).length;
@@ -96,7 +200,7 @@ function qcompare(a, b) {
           for(z in a){
             if(hop.call(a, z)){
               k = b[z];
-              if(a[z] !== k && !qcompare(a[z],k)){
+              if(a[z] !== k && !_(a[z],k)){
                 return false;
               }else if(l === undefined && z !== n[j] && !hop.call(b,z))return false;
               j++;
